a32432ac947eedc91f9e86f99d19d314db987d5b
bigMARAC <marcos.length@gmail.com>
2025-01-13 12:38:02 -0300

    fixes

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,7 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import 'vuetify/styles/main.sass'
+import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 export default createVuetify({
